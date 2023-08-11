aadb5c5fcd18791cfeefc164f9665c1e5f4b1149
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-08-11 19:02:40 -0400

    added logos and basic footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title>Movie Lookup</v-toolbar-title>
+    <v-app-bar app color="primary">
+      <v-toolbar-title>
+        <v-btn to="/" text>
+          <v-img src="../public/favicon.png" max-width="30" />
+          <span class="mx-3">Movie Lookup</span>
+        </v-btn>
+      </v-toolbar-title>
       <v-spacer />
-      <v-btn to="/">Home</v-btn>
-      <v-btn to="/about">About</v-btn>
+      <v-btn to="/" text>Home</v-btn>
+      <v-btn to="/about" text>About</v-btn>
     </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
+    <v-footer class="justify-between" color="primary">
+      <v-container fluid>
+            <v-row>
+                <v-col cols="12" sm="2">
+                    <img src="@/assets/tmdb.svg" />
+                </v-col>
+                <v-spacer />
+                <v-col cols="12" sm="10" class="text-sm-right text-center" >
+                    This product uses the TMDB API but is not endorsed or certified by TMDB.
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-footer>
   </v-app>
 </template>
