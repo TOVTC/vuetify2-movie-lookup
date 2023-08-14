010f8b8f1ff94b7c6477b85578efff759ee80117
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-08-14 13:36:22 -0400

    added placeholder list for search results

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
-    <v-row justify="center" class="my-5">
+    <v-row justify="center" class="ma-5">
       <v-col cols="5">
         <v-form ref="searchForm" v-model="isFormValid" class="text-center" @submit.prevent="search">
           <v-icon color="primary" size="150" class="my-3">mdi-movie-search</v-icon>
-          <v-text-field v-model="searchTerm" :rules="rules" label="Search for a movie" class="my-5"/>
+          <v-text-field v-model="searchTerm" :rules="rules" label="Search for a movie" class="ma-5"/>
           <v-btn type="submit" color="primary">Search</v-btn>
         </v-form>
       </v-col>
@@ -17,19 +17,21 @@ import router from '../router'
 
 export default {
   name: 'HomeView',
-  data: () => ({
-    isFormValid: false,  
-    rules: [
-        input => !!input || 'You must enter a valid search term',
-        input => input.trim().length === 0 ? 'You must enter a valid search term' : true
-      ],
-    searchTerm: '',
-  }),
+  data() {
+    return {
+      isFormValid: false,  
+      rules: [
+          input => !!input || 'You must enter a valid search term',
+          input => input.trim().length === 0 ? 'You must enter a valid search term' : true
+        ],
+      searchTerm: '',
+    }
+  },
   methods: {
     search() {
       this.$refs.searchForm.validate();
       if (this.isFormValid) {
-        router.push({ name: 'about' })
+        router.push({ name: 'search' })
         // router.push({ name: 'results', params: { movieName: movieName.value } })
       }
     }
---
+++ src/views/SearchView.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-row justify="center" class="ma-5">
+      <v-col>
+        <h1>Search Results:</h1>
+      </v-col>
+    </v-row>
+    <v-row class="ma-5">
+      <v-col>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-movie-search',
+          text: 'placeholder'
+        },
+        {
+          icon: 'mdi-movie-search',
+          text: 'placeholder'
+        },
+        {
+          icon: 'mdi-movie-search',
+          text: 'placeholder'
+        },
+      ]
+    }
+  }
+}
+</script>
