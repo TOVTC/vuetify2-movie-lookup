0acaee14d9d0506f075c9c13a5c1b1fe0ea2fe20
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-08-31 17:27:31 -0400

    switched logic to using results JSON directly
    gave up on using classes

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-list v-if="filmOptions.length">
-    <v-list-item v-for="(item, i) in filmOptions" :key="i">
+  <v-list>
+    <v-list-item v-for="(item, i) in movies" :key="i">
       <v-list-item-icon>
         <v-icon v-text="item.posterPath"></v-icon>
       </v-list-item-icon>
       <v-list-item-content>
-        <v-list-item-title v-text="item.generatedTitle"></v-list-item-title>
+        <v-list-item-title v-text="generateTitle(item.title, item.release_date)"></v-list-item-title>
       </v-list-item-content>
     </v-list-item>
   </v-list>
 </template>
 
 <script>
-import MovieOption from '@/models/MovieOption.js';
-
   export default {
     props: ['movies'],
     name: 'MovieList',
@@ -28,26 +26,13 @@ import MovieOption from '@/models/MovieOption.js';
             return title;
           }
             return `${title} (${date.split('-')[0]})`
-        },
-        generatePosterPath(path) {
+      },
+      generatePosterPath(path) {
           if (!path || path === 'null' || path.trim().length === 0) {
             return '/favicon.png';
           } 
           return `https://image.tmdb.org/t/p/original${path}`;
         },
-        updateFilms() {
-        this.movies.forEach(movie => this.filmOptions.push(new MovieOption(
-          movie.id,
-          this.generateTitle(movie.title, movie.release_date),
-          this.generatePosterPath(movie.poster_path)
-        ))
-      );
-      console.log(this.filmOptions)
-      console.log(this.movies)
-      }
     },
-    created() {
-      this.$watch(this.movies, this.updateFilms);
-    }
   }
 </script>
--- src/models/MovieOption.js
+++
@@ -1,7 +0,0 @@
-export class MovieOption {
-    constructor(id, generatedTitle, posterPath) {
-      this.id = id,
-      this.generatedTitle = generatedTitle,
-      this.posterPath = posterPath
-    }
-}
